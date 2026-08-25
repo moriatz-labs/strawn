f10e43813a3c20f3f6d6f061e87a71d0879c177f
Paul M Kallarackal <105142261+Paul-M-Kallarackal@users.noreply.github.com>
2026-08-26 00:37:36 +0530

    chore: remove remaining design-system residue (#30)

--- apps/docs/src/App.tsx
+++ apps/docs/src/App.tsx
@@ -1,10 +1,5 @@
-import { BrowserRouter } from "react-router-dom";
 import { DocsLayout } from "./common/components/DocsLayout";
 
 export function App() {
-  return (
-    <BrowserRouter>
-      <DocsLayout />
-    </BrowserRouter>
-  );
+  return <DocsLayout />;
 }
